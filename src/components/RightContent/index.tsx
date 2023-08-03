52ee4cee3a4ea43b9ec6f6bd3cfa1f037c6dad63
小熊 <>
2023-08-03 20:16:51 +0800

    🐛 fix(compiler): 项目瘦身

--- src/components/RightContent/index.tsx
+++ src/components/RightContent/index.tsx
@@ -1,19 +1,15 @@
 import { QuestionCircleOutlined } from '@ant-design/icons';
-import { SelectLang as UmiSelectLang } from '@umijs/max';
-import React from 'react';
-
+import '@umijs/max';
 export type SiderTheme = 'light' | 'dark';
-
 export const SelectLang = () => {
   return (
-    <UmiSelectLang
+    <QuestionCircleOutlined
       style={{
         padding: 4,
       }}
     />
   );
 };
-
 export const Question = () => {
   return (
     <div
